43d928f1b83d8c033189b6f84f73ed665662607e
syt06162 <syt06162@naver.com>
2021-12-29 18:27:00 +0900

    w02a1 - Activity(Main - Song) 비율코딩, 작은 디바이스 깨짐 해결, 구간반복/랜덤재생 버튼 활성화, 상태바/타이틀바 제거

--- app/src/main/java/com/sherlock/flo/SongActivity.kt
+++ app/src/main/java/com/sherlock/flo/SongActivity.kt
@@ -2,6 +2,7 @@ package com.sherlock.flo
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sherlock.flo.databinding.ActivitySongBinding
 
@@ -27,6 +28,25 @@ class SongActivity : AppCompatActivity() {
             setPlayerStatus(true)
         }
 
+        // 반복 버튼 클릭시 변화 (반복꺼짐(2) - 전체반복(0) - 1개반복(1) - (다시)반복꺼짐)
+        binding.songRepeatOffIv.setOnClickListener {
+            setRepeatStatus(0)
+        }
+        binding.songRepeatOnAllIv.setOnClickListener {
+            setRepeatStatus(1)
+        }
+        binding.songRepeatOnOneIv.setOnClickListener {
+            setRepeatStatus(2)
+        }
+
+        binding.songRandomOffIv.setOnClickListener {
+            setRandomPlayStatus(true)
+        }
+        binding.songRandomOnIv.setOnClickListener {
+            setRandomPlayStatus(false)
+        }
+
+
         // 제목, 가수 이름 받아와서 바꾸기
         if (intent.hasExtra("title") && intent.hasExtra("singer")){
             binding.songMusicTitleTv.text = intent.getStringExtra("title")
@@ -35,14 +55,48 @@ class SongActivity : AppCompatActivity() {
 
     }
 
-    fun setPlayerStatus(isPlaying: Boolean){
+    private fun setPlayerStatus(isPlaying: Boolean){
         if (isPlaying){
             binding.songPlayBtnIv.visibility = View.VISIBLE
             binding.songPauseBtnIv.visibility = View.GONE
         }
         else {
-            binding.songPlayBtnIv.visibility = View.GONE
             binding.songPauseBtnIv.visibility = View.VISIBLE
+            binding.songPlayBtnIv.visibility = View.GONE
+        }
+    }
+
+    private fun setRepeatStatus(isRepeating: Int){
+        // isRepeating 0:전체반복 1:한곡반복 2:반복안함
+        when (isRepeating) {
+            0 -> {
+                binding.songRepeatOffIv.visibility = View.GONE
+                binding.songRepeatOnAllIv.visibility = View.VISIBLE
+                Toast.makeText(this, "전체 음악을 반복합니다.",Toast.LENGTH_SHORT).show()
+            }
+            1 -> {
+                binding.songRepeatOnAllIv.visibility = View.GONE
+                binding.songRepeatOnOneIv.visibility = View.VISIBLE
+                Toast.makeText(this, "현재 음악을 반복합니다.",Toast.LENGTH_SHORT).show()
+            }
+            2 -> {
+                binding.songRepeatOnOneIv.visibility = View.GONE
+                binding.songRepeatOffIv.visibility = View.VISIBLE
+                Toast.makeText(this, "반복을 사용하지 않습니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setRandomPlayStatus(isRandom: Boolean){
+        if (isRandom){
+            binding.songRandomOnIv.visibility = View.VISIBLE
+            binding.songRandomOffIv.visibility = View.GONE
+            Toast.makeText(this, "재생목록을 랜덤으로 재생합니다.",Toast.LENGTH_SHORT).show()
+        }
+        else {
+            binding.songRandomOffIv.visibility = View.VISIBLE
+            binding.songRandomOnIv.visibility = View.GONE
+            Toast.makeText(this, "재생목록을 순서대로 재생합니다.",Toast.LENGTH_SHORT).show()
         }
     }
 
